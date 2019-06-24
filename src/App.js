e9922c6775ee2479bb93cd3ed815c729fb715026
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-23 22:33:14 -0300

    protypes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux'
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 import createNavigator from "~/routes";
 import NavigationService from "~/services/navigation";
 
 class App extends Component {
+  static propTypes = {
+    auth: PropTypes.shape({
+      authChecked: PropTypes.bool,
+      signedIn: PropTypes.bool
+    }).isRequired
+  };
+
   registerService = ref => {
     NavigationService.setTopLevelNavigator(ref);
   };
 
   render() {
-    const { auth } = this.props
+    const { auth } = this.props;
 
-    if (!auth.authChecked) return null
+    if (!auth.authChecked) return null;
 
-    const Routes = createNavigator(auth.signedIn)
+    const Routes = createNavigator(auth.signedIn);
 
     return <Routes ref={this.registerService} />;
   }
