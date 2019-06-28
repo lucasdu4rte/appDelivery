462c15da2732c4555dc0db0e17b88f35da581aa4
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-27 22:55:57 -0300

    Use stack routes

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,41 @@
 import {
   createAppContainer,
-  createSwitchNavigator
-  // createStackNavigator
+  createSwitchNavigator,
+  createStackNavigator
 } from "react-navigation";
 
 import Login from "~/pages/Login";
 import Categories from "~/pages/Categories";
 import Signup from "~/pages/Login/Signup";
-import Category from "~/pages/Category";
+import Products from "~/pages/Products";
 import Sizes from "~/pages/Sizes";
 import Cart from "~/pages/Cart";
+import Confirm from "~/pages/Confirm";
+import Profile from "~/pages/Profile";
 
 function createNavigator(isLoggedIn = false) {
   return createAppContainer(
-    // createStackNavigator(
-    createSwitchNavigator(
-      { Categories, Login, Signup, Category, Sizes, Cart },
+    createStackNavigator(
+      // createSwitchNavigator(
       {
-        initialRouteName: isLoggedIn ? "Categories" : "Login"
+        Categories,
+        Login,
+        Signup,
+        Products,
+        Sizes,
+        Cart,
+        Confirm,
+        Profile
+      },
+      {
+        initialRouteName:
+          isLoggedIn
+          ? // "Confirm"
+            "Categories"
+          :
+          "Login",
+          mode: 'modal',
+          headerMode: 'none'
       }
     )
   );
