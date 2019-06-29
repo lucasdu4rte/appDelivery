6e89aed7366e7d8d47076832dc1831f74110eb08
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-29 03:17:30 -0300

    Rotas complexas

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,69 @@
 import {
   createAppContainer,
   createSwitchNavigator,
-  createStackNavigator
+  createStackNavigator,
+  createDrawerNavigator,
 } from "react-navigation";
 
+// StackNavigator
 import Login from "~/pages/Login";
-import Categories from "~/pages/Categories";
 import Signup from "~/pages/Login/Signup";
+
+// StackNavigator
+import Categories from "~/pages/Categories";
 import Products from "~/pages/Products";
 import Sizes from "~/pages/Sizes";
 import Cart from "~/pages/Cart";
 import Confirm from "~/pages/Confirm";
+
+// Drawer
 import Profile from "~/pages/Profile";
+
+const AuthStack = createStackNavigator(
+  {
+    Login,
+    Signup,
+  },
+  {
+    initialRouteName: "Login",
+    headerMode: "none"
+  }
+)
+
+const MainStack = createStackNavigator(
+  {
+    Categories,
+    Products,
+    Sizes,
+    Cart,
+    Confirm,
+  },
+  {
+    initialRouteName: "Categories",
+    headerMode: "none"
+  }
+)
+
+const Drawer = createDrawerNavigator({
+  Home: MainStack,
+  Carrinho: Cart,
+  Perfil: Profile,
+  Sair: Login,
+}, {
+  initialRouteName: "Home"
+})
 
 function createNavigator(isLoggedIn = false) {
   return createAppContainer(
     createStackNavigator(
-      // createSwitchNavigator(
       {
-        Categories,
-        Login,
-        Signup,
-        Products,
-        Sizes,
-        Cart,
-        Confirm,
-        Profile
+        Drawer,
+        AuthStack,
       },
       {
-        initialRouteName:
-          isLoggedIn
-          ? // "Confirm"
-            "Categories"
-          :
-          "Login",
-          mode: 'modal',
-          headerMode: 'none'
+        initialRouteName: isLoggedIn ? "Drawer" : "AuthStack",
+        mode: "modal",
+        headerMode: "none"
       }
     )
   );
