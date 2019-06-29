7271e0d19506a0cec4126453fd612abd30de74ae
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-29 03:16:29 -0300

    Filter by id

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import moment from "moment"
-import locale from 'moment/locale/pt-br'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import "moment/locale/pt-br";
+import { connect } from 'react-redux';
 
 import {
   Container,
@@ -9,94 +10,77 @@ import {
   Info,
   Description,
   PrepareTime,
-  Price,
+  Price
 } from "./styles";
 
 import Header from "~/components/Header";
+import api from "~/services/api";
 
-moment.locale('pt-br', locale)
+moment.locale("pt-br");
 
-class Profile extends Component {
-  handleProfilePress = category => {
-    // const {navigation} = this.props
-    // console.tron.log('ONPRESS')
-    // navigation.navigate('Sizes', { category })
-  };
+const Profile = props => {
+  const { navigation, profile } = props;
+  // const category = navigation.getParam("category");
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.tron.log(profile);
 
-  render() {
-    const { navigation } = this.props;
-    // const category = navigation.getParam("category");
-    const items = [
-      {
-        id: Math.random(),
-        photo_url: "",
-        // datetime: "Ontem às 17h",
-        datetime: moment().add(1, 'week').toNow(),
-        description: "Pedido #1",
-        price: 42
-      },
-      {
-        id: Math.random(),
-        photo_url: "",
-        // datetime: "Há 1 semena",
-        datetime: moment().add(2, 'weeks').toNow(),
-        description: "Pedido #2",
-        price: 42
-      },
-      {
-        id: Math.random(),
-        photo_url: "",
-        // datetime: "Há 2 meses",
-        datetime: moment().add(1, 'months').toNow(),
-        description: "Pedido #3",
-        price: 29
-      }
-    ];
-    return (
-      <Container>
-        <Header title="Carrinho" leftComponent="R$107,00" />
-        <OrderList
-          data={items}
-          keyExtractor={item => String(item.id)}
-          renderItem={({ item }) => (
-            <Order
-              // onPress={() => this.handleCategoryPress(category)}
-              style={{
-                shadowColor: "#bdc3c7",
-                shadowOffset: {
-                  width: 0,
-                  height: 7
-                },
-                shadowOpacity: 0.43,
-                shadowRadius: 9.51,
-                elevation: 15
-              }}
-            >
-              <Info>
-                <Description>{item.description}</Description>
-                <PrepareTime>{item.datetime}</PrepareTime>
-                <Price>
-                  R$
-                  {Number(item.price)
-                    .toFixed(2)
-                    .replace(".", ",")}
-                </Price>
-              </Info>
-            </Order>
-          )}
-        />
-      </Container>
-    );
-  }
-}
+  useEffect(() => {
+    api.get(`/orders?filter_by_user_id=${profile.id}`).then(({ data }) => {
+      setOrders(
+        data
+          .map(order => ({
+            ...order,
+            totalFormated: Number(order.total)
+              .toFixed(2)
+              .replace(".", ","),
+            orderDateFormated: moment(order.order_date).fromNow()
+          }))
+          .sort((a, b) => moment(b.order_date) - moment(a.order_date))
+      );
+      setLoading(false);
+    });
+  }, []);
 
-// Profile.navigationOptions = {
-//   title: 'Selecione um tipo',
-//   headerRight: (
-//     <TouchableOpacity onPress={() => {}}>
-//       <Icon name="shopping-bag" size={16} style={styles.icon} />
-//     </TouchableOpacity>
-//   )
-// }
+  return (
+    <Container>
+      <Header title="Perfil" leftComponent="" />
+      <OrderList
+        data={orders}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <Order
+            // onPress={() => this.handleCategoryPress(category)}
+            style={{
+              shadowColor: "#bdc3c7",
+              shadowOffset: {
+                width: 0,
+                height: 7
+              },
+              shadowOpacity: 0.43,
+              shadowRadius: 9.51,
+              elevation: 15
+            }}
+          >
+            <Info>
+              <Description>Pedido #{item.id}</Description>
+              <PrepareTime>{item.orderDateFormated}</PrepareTime>
+              <Price>
+                R$
+                {item.totalFormated}
+              </Price>
+            </Info>
+          </Order>
+        )}
+      />
+    </Container>
+  );
+};
 
-export default Profile;
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+)(Profile);
