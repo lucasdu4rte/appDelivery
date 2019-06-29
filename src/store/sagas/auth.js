99cd023579d374a58b4dc5570f57f1c446d5ca27
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-29 12:23:09 -0300

    Not permit admin in app

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -5,7 +5,28 @@ import NavigationService from "~/services/navigation";
 
 import { Creators as AuthActions } from "../ducks/auth";
 import { Creators as ProfileActions } from "../ducks/profile";
-import { AsyncStorage } from "react-native";
+import { AsyncStorage, Alert } from "react-native";
+
+function alertToAdmin() {
+  Alert.alert(
+    "Olá admin! Está área é exclusiva para seu cliente",
+    "Por favor, acesse a plataforma web com esta conta",
+    [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+    { cancelable: false }
+  );
+}
+
+function* persistTokenProfile ({token, profile}) {
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+  yield AsyncStorage.setItem("@Delivery:token", token);
+  yield AsyncStorage.setItem(
+    "@Delivery:profile",
+    JSON.stringify(profile)
+  );
+
+  return
+}
 
 export function* init() {
   const token = yield call([AsyncStorage, "getItem"], "@Delivery:token");
@@ -26,22 +47,45 @@ export function* init() {
 
 export function* login(action) {
   try {
+    if (action.payload && !action.payload.email.length && !action.payload.password.length) {
+      yield all([
+        put(AuthActions.loginFailure(true))
+      ]);
+      return
+    }
+
     const {
       data: { token, user }
     } = yield call(api.post, "/sessions", action.payload);
 
     api.defaults.headers.common.Authorization = `Bearer ${token}`;
     yield call([AsyncStorage, "setItem"], "@Delivery:token", token);
-    yield call([AsyncStorage, "setItem"], "@Delivery:profile", JSON.stringify(user));
+    yield call(
+      [AsyncStorage, "setItem"],
+      "@Delivery:profile",
+      JSON.stringify(user)
+    );
+    // yield call(persistTokenProfile, { token, profile: user })
+
+    if (user.type !== 'admin') {
+      yield all([
+        put(AuthActions.loginSuccess({ token })),
+        put(ProfileActions.setProfileData(user))
+      ]);
+
+      NavigationService.navigate("Categories");
+      return
+    }
 
     yield all([
-      put(AuthActions.loginSuccess({ token })),
-      put(ProfileActions.setProfileData(user))
+      put(AuthActions.loginFailure(true))
     ]);
 
-    NavigationService.navigate("Categories");
+    alertToAdmin()
   } catch (err) {
-    put(AuthActions.loginFailure(true));
+    yield all([
+      put(AuthActions.loginFailure(true))
+    ]);
   }
 }
 
@@ -52,11 +96,18 @@ export function* signup(action) {
     } = yield call(api.post, "/users", action.payload);
 
     api.defaults.headers.common.Authorization = `Bearer ${token}`;
+    yield call([AsyncStorage, "setItem"], "@Delivery:token", token);
+    yield call(
+      [AsyncStorage, "setItem"],
+      "@Delivery:profile",
+      JSON.stringify(user)
+    );
 
     yield all([
       put(ProfileActions.setProfileData(user)),
       put(AuthActions.loginSuccess({ token }))
     ]);
+
     navigate("Categories");
   } catch (err) {
     yield put(
